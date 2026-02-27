a03719164caac9c83452cc47b0a2ecee1460f4b2
niiwade <niilantewade77@gmail.com>
2026-02-27 14:59:16 +0000

    added this

--- src/lib/OrbitControls.js
+++ src/lib/OrbitControls.js
@@ -9,7 +9,7 @@ import {
 	Plane,
 	Ray,
 	MathUtils
-} from 'https://unpkg.com/three@latest?module';
+} from './three.module.js';
 
 // OrbitControls performs orbiting, dollying (zooming), and panning.
 // Unlike TrackballControls, it maintains the "up" direction object.up (+Y by default).
